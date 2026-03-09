aaf691146240b62915e0b6557e1b88c790732d38
ruthesh-premkanth <113876617+ruthesh-premkanth@users.noreply.github.com>
2026-03-09 19:33:09 +0530

    Refactor Vite config for base path and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/PixelPunch/',
+
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
-    base: '/PixelPunch/',
+  ],
+
   build: {
     outDir: 'dist'
-  }
-  ],
+  },
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
