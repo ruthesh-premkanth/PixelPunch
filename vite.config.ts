581bd62dc477cc7d8f567c253f3e1bacc670dfdf
Ruthesh Chintham <rutheshpremkanth@gmail.com>
2026-03-09 19:50:52 +0530

    fix vite build for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/PixelPunch/',
+  base: "./",
 
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+  assetsInclude: ["**/*.svg", "**/*.csv"],
+});
