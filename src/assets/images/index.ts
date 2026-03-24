2fb43b8a3fdf3f6bab5810da49e0e14eecb9c6ba
Ruthesh Chintham <rutheshpremkanth@gmail.com>
2026-03-24 14:00:57 +0530

    Updated the images path

--- src/assets/images/index.ts
+++
@@ -1,6 +0,0 @@
-import ruthesh from "./Ruthesh.jpg";
-import anuja from "./AnujaKasari.jpg";
-import purneswari from "./Purneswari.png";
-import yeswithi from "./Yeswithi.jpg";
-
-export { ruthesh, anuja, purneswari, yeswithi };
