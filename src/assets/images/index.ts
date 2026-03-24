9384b852f5e67cf109f18b4934b8dfcd80d60161
Ruthesh Chintham <rutheshpremkanth@gmail.com>
2026-03-24 13:44:06 +0530

    added images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import ruthesh from "./Ruthesh.jpg";
+import anuja from "./AnujaKasari.jpg";
+import purneswari from "./Purneswari.png";
+import yeswithi from "./Yeswithi.jpg";
+
+export { ruthesh, anuja, purneswari, yeswithi };
